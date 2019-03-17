3ccb4989dfe4b578c4ac6636de80f21bc9ffe719
farreedd <mdfareed969@hotmail.com>
2019-03-17 04:17:42 +0300

    completed application

---
+++ precache-manifest.975a0c83498bbdd7ebbba356b992d011.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "1939670321dc8f1a2789",
+    "url": "/static/js/main.31957355.chunk.js"
+  },
+  {
+    "revision": "4985315803a1902a82bd",
+    "url": "/static/js/2.7cb68033.chunk.js"
+  },
+  {
+    "revision": "1939670321dc8f1a2789",
+    "url": "/static/css/main.9670b506.chunk.css"
+  },
+  {
+    "revision": "4985315803a1902a82bd",
+    "url": "/static/css/2.096a3f30.chunk.css"
+  },
+  {
+    "revision": "62abc3dd1fff16c0c3abd73e3200a03a",
+    "url": "/index.html"
+  }
+];
